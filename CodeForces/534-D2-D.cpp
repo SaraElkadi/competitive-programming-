0e6abfd47d113fc5c5694d9981feb5350cadc236
SaraElkadi <s.elkadi97@gmail.com>
2018-01-31 13:07:36 +0200

    Create 534-D2-D.cpp

---
+++ CodeForces/534-D2-D.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n],b[200005]= {0},d[3]= {0};
+    vector<vector<int> > v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+        d[a[i]%3]++;
+        b[a[i]]++;
+        v[a[i]].push_back(i+1);
+    }
+
+    if(d[0]<d[1]||d[1]<d[2]||d[0]-d[1]>1||d[0]-d[2]>1)
+    {
+        cout<<"Impossible";
+        return 0;
+    }
+    bool f=1;
+    int q=0;
+    int c[200005]= {0};
+    vector<int> vv;
+    while(q<n&&f)
+    {
+        for(int i=0;i<n&&q<n; i++)
+        {
+            if(v[i].size()>c[i])
+            {
+                vv.push_back(v[i][c[i]]);
+                q++;
+                c[i]++;
+            }
+            else{
+             if(i>2)
+                i-=4;
+             else{
+                f=0;
+                break;
+             }
+            }
+
+        }
+    }
+    if(!f)
+        cout<<"Impossible";
+    else{
+       cout<<"Possible"<<endl;
+       for(int i=0;i<vv.size();i++)
+            cout<<vv[i]<<" ";
+    }
+    return 0;
+}
