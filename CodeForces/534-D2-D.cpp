d9dfe94b00956c19cc8e95c27e02a0beaa36f7c2
SaraElkadi <s.elkadi97@gmail.com>
2018-01-31 13:15:57 +0200

    Update 534-D2-D.cpp

--- CodeForces/534-D2-D.cpp
+++ CodeForces/534-D2-D.cpp
@@ -4,52 +4,45 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,k=0,c[200005]= {0};
     cin>>n;
-    int a[n],d[3]= {0};
+    int a[n];
     vector<vector<int> > v(n);
+    vector<int> vv;
     for(int i=0; i<n; i++)
     {
         cin>>a[i];
-        d[a[i]%3]++;
         v[a[i]].push_back(i+1);
     }
-
-    if(d[0]<d[1]||d[1]<d[2]||d[0]-d[1]>1||d[0]-d[2]>1)
-    {
-        cout<<"Impossible";
-        return 0;
-    }
     bool f=1;
-    int q=0;
-    int c[200005]= {0};
-    vector<int> vv;
-    while(q<n&&f)
+    while(k<n&&f)
     {
-        for(int i=0;i<n&&q<n; i++)
+        for(int i=0; i<n&&k<n; i++)
         {
             if(v[i].size()>c[i])
             {
                 vv.push_back(v[i][c[i]]);
-                q++;
+                k++;
                 c[i]++;
             }
-            else{
-             if(i>2)
-                i-=4;
-             else{
-                f=0;
-                break;
-             }
+            else
+            {
+                if(i>2)
+                    i-=4;
+                else
+                {
+                    f=0;
+                    break;
+                }
             }
-
         }
     }
     if(!f)
         cout<<"Impossible";
-    else{
-       cout<<"Possible"<<endl;
-       for(int i=0;i<vv.size();i++)
+    else
+    {
+        cout<<"Possible"<<endl;
+        for(int i=0; i<vv.size(); i++)
             cout<<vv[i]<<" ";
     }
     return 0;
