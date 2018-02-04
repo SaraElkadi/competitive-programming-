58247e72420d4caf519a8f688e21a1ba774decb8
SaraElkadi <s.elkadi97@gmail.com>
2018-02-04 19:06:18 +0200

    Update 96-D2-D.cpp

--- CodeForces/96-D2-D.cpp
+++ CodeForces/96-D2-D.cpp
@@ -1,38 +1,46 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int oo=1e9+10;
+const long long oo=1e18+10;
 long long n,m,x,y,u,v,w;
-vector<vector<long long>> adj(1005,vector<long long>(1005,oo));
 long long t[1005],c[1005];
-vector<vector<int> > d;
-vector<int> dijkstra(long long src)
+vector<vector<long long> > d;
+struct edge {
+    long long from, to, w;
+
+    edge(long long from, long long to, long long w): from(from), to(to), w(w) {}
+
+    bool operator < (const edge & e) const {
+        return w > e.w;
+    }
+};
+vector<vector<edge> >adj(1005);
+vector<long long> dijkstra(long long src)
 {
-    vector<int> vis(1005,0),dist(1005,oo),prev(1005,-1);
+    vector<long long> dist(1005,oo),prev(1005,-1);
     dist[src]=0;
-    while(1)
-    {
-        long long val=oo,idx=-1;
-        for(int i=0; i<n; i++)
-        {
-            if(!vis[i]&&dist[i]<val)
-                val=dist[i],idx=i;
-        }
-        if(idx==-1)
-            break;
-        vis[idx]=1;
-        for(int i=0; i<n; i++)
-        {
-            val=dist[idx]+adj[idx][i];
-            if(dist[i]>val)
-               dist[i]=val,prev[i]=idx;
+    priority_queue<edge> q;
+    q.push(edge(-1,src,0));
+   while( !q.empty() ) {
+        edge e = q.top();   q.pop();
+
+        if(e.w > dist[e.to]) continue;   // some other state reached better
+
+        prev[ e.to ] = e.from;
+
+        for(int j=0;j<adj[e.to].size();j++) {
+            edge ne = adj[e.to][j];
+            if( dist[ne.to] > dist[ne.from] + ne.w ) {
+                ne.w = dist[ne.to] = dist[ne.from] + ne.w;
+                q.push( ne );
+            }
         }
     }
     return dist;
 }
-int dij(int src,int dest=-1)
+long long dij(int src,int dest=-1)
 {
-    vector<int> vis(1005,0),dist(1005,oo),prev(1005,-1);
+    vector<long long> vis(1005,0),dist(1005,oo),prev(1005,-1);
     dist[src]=0;
     while(1)
     {
@@ -57,20 +65,21 @@ int dij(int src,int dest=-1)
 
 int main()
 {
-    scanf("%d %d %d %d",&n,&m,&x,&y);
+    //cin>>n>>m>>x>>y;
+    scanf("%lld %lld %lld %lld",&n,&m,&x,&y);
     x--,y--;
     for(int i=0; i<m; i++)
     {
-        scanf("%d %d %d",&u,&v,&w);
+        scanf("%lld %lld %lld",&u,&v,&w);
         u--,v--;
-        adj[u][v]=min(adj[u][v],w);
-        adj[v][u]=min(adj[v][u],w);
+        adj[u].push_back(edge{u,v,w});
+        adj[v].push_back(edge{v,u,w});
     }
     for(int i=0; i<n; i++)
     {
-        scanf("%d %d",&t[i],&c[i]);
+        scanf("%lld %lld",&t[i],&c[i]);
     }
-    for(int i=0;i<n;i++){
+    for( int i=0;i<n;i++){
         d.push_back(dijkstra(i));
         for(int j=0;j<d[i].size();j++){
             if(d[i][j]<=t[i])
@@ -79,9 +88,9 @@ int main()
                 d[i][j]=oo;
         }
     }
-    int ans=dij(x,y);
+    long long ans=dij(x,y);
     if(ans!=oo)
-      printf("%d",ans);
+      printf("%lld",ans);
     else
         puts("-1");
     return 0;
