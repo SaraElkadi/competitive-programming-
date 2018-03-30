fcaaa8a141df3e7fa705a4282cf3ea1f10d5d744
SaraElkadi <s.elkadi97@gmail.com>
2018-03-30 13:14:09 +0200

    Create 12124.cpp

---
+++ UVA/12124.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,n,k,q,p;
+    string name,type;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>k;
+        vector<vector<pair<int,int>>> v(1005);
+        map<string,int> mp;
+        int cnt=1,l=0,r=1e9,ans=0,mid,mn,sum;
+        while(n--)
+        {
+            cin>>name>>type>>p>>q;
+            if(!mp[name])
+            {
+                mp[name]=cnt++;
+            }
+            int x=mp[name];
+            v[x].push_back(make_pair(p,q));
+        }
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            sum=0;
+            for(int i=1; i<cnt; i++)
+            {
+                mn=1e9;
+                for(int j=0; j<v[i].size(); j++)
+                {
+                    if(v[i][j].second>=mid)
+                        mn=min(mn,v[i][j].first);
+                }
+                sum+=mn;
+                if(sum>k)
+                    break;
+            }
+            if(sum<=k)
+                l=mid+1,ans=max(ans,mid);
+            else
+                r=mid-1;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
