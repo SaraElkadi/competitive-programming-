d3993f34953d70a48ad739f933f8354d39896636
SaraElkadi <s.elkadi97@gmail.com>
2018-04-25 10:52:25 +0200

    Update 11635.cpp

--- UVA/11635.cpp
+++ UVA/11635.cpp
@@ -2,16 +2,25 @@
 
 using namespace std;
 const int OO=1e9+7;
-struct edge {
+const int N=10005;
+int n,h,a,m,u,x,y,w;
+
+struct edge
+{
     int from, to, w;
 
     edge(int from, int to, int w): from(from), to(to), w(w) {}
 
-    bool operator < (const edge & e) const {
+    bool operator < (const edge & e) const
+    {
         return w > e.w;
     }
 };
-vector< vector< edge > > adj(10005);
+
+vector< vector< edge > > adj(N);
+vector<vector<int> > v(N);
+bool vis[N];
+
 vector<int> dij( int src)
 {
     int n =adj.size();
@@ -21,16 +30,20 @@ vector<int> dij( int src)
     priority_queue<edge> q;
     q.push( edge(-1, src, 0) );
 
-    while( !q.empty() ) {
-        edge e = q.top();   q.pop();
+    while( !q.empty() )
+    {
+        edge e = q.top();
+        q.pop();
 
         if(e.w > dist[e.to]) continue;
 
         prev[ e.to ] = e.from;
 
-        for(int j=0;j<adj[e.to].size();j++) {
+        for(int j=0; j<adj[e.to].size(); j++)
+        {
             edge ne = adj[e.to][j];
-            if( dist[ne.to] > dist[ne.from] + ne.w ) {
+            if( dist[ne.to] > dist[ne.from] + ne.w )
+            {
                 ne.w = dist[ne.to] = dist[ne.from] + ne.w;
                 q.push( ne );
             }
@@ -38,51 +51,66 @@ vector<int> dij( int src)
     }
     return dist;
 }
-bool ff=0;
-bool f[10005];
-int ans,N;
-void solve(int n,int cnt,vector<int> vis){
-    vis[n]=1;
-    vector<int> d=dij(n);
-    if(d[N]<=600){
-        ff=1;
-        ans=min(cnt,ans);
-        return ;
+
+int bfs()
+{
+    queue<int> q;
+    q.push(1);
+    int dep = 0, sz = 1;
+    for (; !q.empty(); ++dep, sz = q.size())
+    {
+        while (sz--)
+        {
+            int c = q.front();
+            q.pop();
+            if (c == n)
+            {
+                return dep-1;
+
+            }
+            for (int i = 0; i < v[c].size(); i++)
+                if (!vis[v[c][i]])
+                    vis[v[c][i]] = 1, q.push(v[c][i]);
+        }
     }
-    for(int i=0;i<d.size();i++){
-        if(!vis[i]&&f[i]&&d[i]!=OO&&d[i]<=600)
-             solve(i,cnt+1,vis);
-    }
+    return -1;
 }
 int main()
 {
-    int n,h,a,m,u,v,w;
-    while(cin>>n&&n){
-            ff=0;
-            ans=OO;
-        for(int i=0;i<10005;i++){
-            f[i]=0;
+    while(cin>>n&&n)
+    {
+        vector<int> s;
+        for(int i=0; i<N; i++)
+        {
+            vis[i]=0;
             adj[i].clear();
+            v[i].clear();
         }
         cin>>h;
-        for(int i=0;i<h;i++){
+        for(int i=0; i<h; i++)
+        {
             cin>>a;
-            f[a-1]=1;
+            s.push_back(a);
         }
+        if(find(s.begin(),s.end(),1)==s.end())
+            s.push_back(1);
+        if(find(s.begin(),s.end(),n)==s.end())
+            s.push_back(n);
         cin>>m;
-        for(int i=0;i<m;i++){
-            cin>>u>>v>>w;
-            u--;v--;
-            adj[u].push_back(edge{u,v,w});
-            adj[v].push_back(edge{v,u,w});
+        for(int i=0; i<m; i++)
+        {
+            cin>>x>>y>>w;
+            adj[x].push_back(edge {x,y,w});
+            adj[y].push_back(edge {y,x,w});
         }
-        vector<int> vis(10005,0);
-        N=n-1;
-       solve(0,0,vis);
-        if(!ff)
-            cout<<-1<<endl;
-        else
-            cout<<ans<<endl;
+        for(int i=0; i<s.size(); i++)
+        {
+            vector<int> d=dij(s[i]);
+            for(int j=1; j<=n; j++)
+                if(d[j]<=600)
+                    v[s[i]].push_back(j);
+        }
+        cout<<bfs()<<endl;
     }
     return 0;
 }
