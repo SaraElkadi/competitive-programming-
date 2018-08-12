9a774f62507128139c0df3fee4609d24148461e8
Sara Elkady <s.elkadi97@gmail.com>
2018-08-12 21:53:13 +0200

    Update 542.cpp

--- UVA/542.cpp
+++ UVA/542.cpp
@@ -1,3 +1,17 @@
+/*
+As the number of teams is 16, then there are 4 stages. at each stage the teams divide into groups 
+in first stage, every group consist of (2^1) team  and In the second stage, every group consist of (2^2) team ,
+and so on, (2^3),(2^4) in third and forth stage.
+
+apply the same idea in the solution, get the probability of winning for each team in the first stage, 
+Then in the second stage calculate the probability of winning if he played with all teams in his group 
+except teams that already played with in the previous stages (used bool visit array to handle this),
+and so on untill finish the four stages.
+
+P.S: To calculate the probability of winning in any stage, we need the probabilities of previous stage only,
+So I used array of two rows only to store probabilities of this and previous stage.
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
